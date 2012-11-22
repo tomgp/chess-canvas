e02361ede9cdf38dd0d4b699016f9983c8a0ca59
Tom <tom.g.pearson@gmail.com>
2012-11-22 12:34:02 +0000

    creating repo

---
+++ js/ChessGame.js
@@ -0,0 +1,33 @@
+//decompressed game
+(function(window) {
+	function ChessGame(pgn) {
+		this.initialize(pgn);
+	}
+	var p = ChessGame.prototype;
+
+		// constructor:
+	p.initialize = function(pgn) {
+		console.log(pgn);
+		p.metadata = buildMetaData(pgn);
+		p.turns = buildBoardStates(pgn);
+	}
+
+	function buildMetaData(pgn){
+		return {};
+	}
+
+	function buildBoardStates(pgn){
+		var boards = [];
+		//build board 0, the default state
+		//split moves
+		//for each move, 
+		//	duplicate the previous board state, 
+		//	apply the change
+		//	stash the board in the boards array
+		return boards;
+	}
+	if(!window.chessvis){
+		window.chessvis = {};
+	}
+	window.chessvis.ChessGame = ChessGame;
+}(window));
