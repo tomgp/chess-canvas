32098c69bc2634fcb39425a95516b4248c7de577
Tom <tom.g.pearson@gmail.com>
2012-12-11 13:35:45 +0000

    new generated data
    giving different results :(

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 var util = require('util');
 var fs = require('fs');
-var ch = require('/Users/tompearson/Sites/vendor/chess.js')
+var ch = require('/Users/tompearson/Sites/vendor/chess.js');
+
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
 
 var games = {};
 
+init();
+
 var players = [
-	/*{
+	{
 		name:'Anand', data:'pgn/Anand.pgn'
-	},
+	}/*,
 	{
 		name:'Carlsen', data:'pgn/Carlsen.pgn' //has errors
 	},
 	{
 		name:'Nakamura', data:'pgn/Nakamura.pgn'
-	},*/
+	},
 	{
 		name:'Gelfand', data:'pgn/Gelfand.pgn'
-	}
+	}*/
 ];
 
 for (var p = 0; p<players.length; p++){
@@ -24,11 +31,13 @@ for (var p = 0; p<players.length; p++){
 	players[p].frequencies = runGames(players[p]);
 	fen_frequencies = JSON.stringify(players[p].frequencies.fens);
 	move_frequencies = JSON.stringify(players[p].frequencies.moves);
+
+	//maybe trim FEN data, remove anything configuration that occurs only once
 	var out_file = fs.openSync('generated_data/' + players[p].name + '_fen.json', 'w');
 		fs.writeSync(out_file, fen_frequencies);
 		fs.closeSync(out_file);
 	out_file = fs.openSync('generated_data/' + players[p].name + '_moves.json', 'w');
-		fs.writeSync(out_file, move_frequencies);
+		fs.writeSync(out_file, players[p].name + '_moves =' + move_frequencies);
 		fs.closeSync(out_file);
 }
 
@@ -39,11 +48,39 @@ function runGames(player){
 	var moves = []
 	var fens = {};
 	for (var i=0; i<games_list.length; i++ ){
+		var meta_data = new GameMetaData(games_list[i]);
+
+		//is the player black or white?		
+		//did the player win, lose or draw?
+		if(String(meta_data.getData().White).indexOf(player.name) > -1){
+			var white = true;
+			if(meta_data.getData().Result == "0-1"){
+				var win = -1;
+			}else if(meta_data.getData().Result == "1-0"){
+				win = 1;
+			}else{
+				win = 0;
+			}
+		}else{
+			white = false;
+			if(meta_data.getData().Result == "0-1"){
+				var win = 1;
+			}else if(meta_data.getData().Result == "1-0"){
+				win = -1;
+			}else{
+				win = 0;
+			}
+		}
+
+		var win = 1; //1,0,-1
 		if(games_list[i] != ""){
 			var c = new ch.Chess();
 			c.load_pgn(games_list[i]);
+			util.puts("loaded");
 			var game_moves = c.history();
+			util.puts("got history");
 			c.reset();
+			util.puts("reset");
 			for(var m=0; m<game_moves.length; m++){
 				if(!moves[m]){
 					moves[m] = {};
@@ -52,7 +89,6 @@ function runGames(player){
 					moves[m][game_moves[m]] = 0;
 				}
 				moves[m][game_moves[m]] ++;
-
 				c.move(game_moves[m]);
 				var fen = c.fen();
 				if(!fens[fen]){
@@ -68,18 +104,47 @@ function runGames(player){
 		moves[m].total = Object.keys(moves[m]).length;
 	}
 	var fen_array = [];
+	var cutoff_frequency = 0;
 	for(var f in fens){
-		fen_array.push({
-			fen:f,
-			frequency:fens[f]
-		});
+		if(fens[f] > cutoff_frequency){
+			fen_array.push({
+				fen:f,
+				frequency:fens[f]
+			});
+		}
 	}
 	fen_array.sort(function(a,b){
 		return b.frequency - a.frequency;
 	});
+
+
 	return {
 		moves: moves,
 		fens:fen_array,
+		player_positions:{
+			white:{
+				game_won:{},
+				game_lost:{}
+			},
+			black:{
+				game_won:{},
+				game_lost:{}
+			}
+		},
+		career_results:{
+			wins:{
+				white:0,
+				black:0
+			},
+			draw:{
+				white:0,
+				black:0
+			},
+			loss:{
+				white:0,
+				black:0
+			}
+		}
 	}
 	util.puts("run through " + player.name);
 }
@@ -92,3 +157,36 @@ function loadPGN(file_name){
 	var games_list = games_string.replace(/\[Event/g, "!NEW GAME![Event").split('!NEW GAME!');
 	return games_list;
 }
+
+//-- 
+
+	function GameMetaData(pgn_string){
+		this.initialise(pgn_string);
+	}
+
+	function init(){
+		var p = GameMetaData.prototype;
+		p.data;
+
+		p.initialise = function(pgn){
+			this.data = {};
+			var lines = pgn.split(/\n/); 		//go through the lines of the PGN string
+			for (var i = 0; i<lines.length ; i++){
+				//grab everything that is enclosed by [] on each line
+				var meta_regex = /\[(\w+) "(.+)"\]/g;
+				var matches = meta_regex.exec(lines[i]);
+				if(matches){
+					this.data[matches[1]] = matches[2];
+				}
+			}
+		}
+
+		p.addNalimovPoint = function(move_num, fen){
+			this.data.nalimovPoint = move_num;
+			this.data.nalimovFEN = fen;
+		}
+
+		p.getData = function(){
+			return this.data;
+		}
+	}
