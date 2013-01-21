6b7dd8ee4449788ccff751008bee08c5ebd9e9ca
Tom Pearson <tompearson@Toms-MacBook-Air.local>
2013-01-21 10:32:37 +0000

    factored out tree building method

--- node/generate_d3_tree_extended.js
+++ node/generate_d3_tree_extended.js
@@ -15,13 +15,10 @@ id:{
 	"weight":1 //how many times in the creation process the node has been visited
 }
 */
-
 var util = require('util');
 var fs = require('fs');
-var ch = require('/Users/tompearson/Sites/vendor/chess.js');
 var d3 = require('d3');
-
-var chess_utils = require('chess_utils.js');
+var chess_utils = require('chess_utils');
 
 var start_time = new Date().valueOf()/1000;
 
@@ -30,97 +27,15 @@ var openings_text = fs.readFileSync(openings_spreadsheet,'utf8');
 var openings_list = d3.csv.parse(openings_text);
 
 util.puts("starting v2 (t:"+start_time+")");
-
-var tree_data = build_tree(openings_list);
-
+//generate the tree
+var tree_data = JSON.stringify( chess_utils.build_move_tree(openings_list) );
 //write out the move tree
 var out_file = fs.openSync('../generated_data/d3_move_tree.json', 'w');
-	fs.writeSync(out_file, JSON.stringify(tree_data));
+	fs.writeSync(out_file, tree_data);
 	fs.closeSync(out_file);
 
 out_file = fs.openSync('../generated_data/d3_move_tree.live.json', 'w');
-	fs.writeSync(out_file, "var openings_tree = " + JSON.stringify(tree_data));
+	fs.writeSync(out_file, "var openings_tree = " + tree_data);
 	fs.closeSync(out_file);
 
-var stop_time = new Date().valueOf()/1000;
-util.puts("stopping (t:"+stop_time +")");
-var ex_time = stop_time - start_time
-util.puts("done in " + ex_time + " s");
-
-
-
-function build_tree(sequences){
-	var move_tree = {
-		name:"move 0", 
-		children:[]
-	};
-	var node_lookup = {}
-
-	for(var i = 0; i<sequences.length;i++){
-		var game = new ch.Chess();
-		game.load_pgn(sequences[i].moves);
-		var history = game.history();
-		var current_node = move_tree;
-		var node_name = "";
-		for(var m = 0; m<history.length; m++){
-			var move = history[m];
-			var next_index = -1;
-			//get the index of the node with this move as its name
-			node_name = node_name + "_" + move;	 // SHOULD give us a unique and repeatable id
-			for(var c = 0; c < current_node.children.length; c++){
-				if(current_node.children[c].move == move){
-					next_index = c;
-				}
-			}
-			//if it doesn't exist create it
-			if(next_index < 0){	
-				current_node.children.push({
-					name:node_name,
-					move:move,
-					children:[]
-				});
-				next_index = current_node.children.length - 1; 
-			}
-			if(!node_lookup[node_name]){
-				node_lookup[node_name] = {
-					weight:0,
-					opening_groups:[],
-					opening_ending:""
-				};
-			}	
-			//set it as the current node
-			current_node = current_node.children[next_index];
-			//update counts
-			node_lookup[node_name].weight ++;
-			node_lookup[node_name].opening_groups.push(sequences[i].ECO[0]);
-			if(m == history.length-1){
-				node_lookup[node_name].opening_ending = sequences[i].name + "(" +  sequences[i].ECO + ")";
-			}
-			if(next_index < 0){
-				util.puts("ERROR! current_node = " + current_node);
-			}
-		}
-	}
-
-	//de duplicate all the opening group arrays
-	for(var n in node_lookup){
-		node_lookup[n].opening_groups = uniqueArray(node_lookup[n].opening_groups);
-	}
-
-	var output_struct = {
-		tree:move_tree,
-		lookup:node_lookup
-	};
-
-	return output_struct;
-}
-
-function uniqueArray(unordered) { //return a de-duplicated version of an array
-	var result = [];
-	var object = {};
-	unordered.forEach(function(item) {
-		object[item] = null;
-	});
-	result = Object.keys(object);
-	return result.sort();
-}
+util.puts("done in " + (( new Date().valueOf()/1000 ) - start_time ) + " s");
