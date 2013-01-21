aecf060270021379c21964f6ddb34720004922ef
Tom Pearson <tompearson@Toms-MacBook-Air.local>
2013-01-21 10:56:04 +0000

    removed an empty file, added "pgn to tree" , added some stuff to the chess utils library
    created a node script for converting a PGN file to a move tree suitable
    for d3 rendering
    chess utils library now handles the PGN reading and meta data grabbing

---
+++ node/pgn_to_d3_tree.js
@@ -0,0 +1,40 @@
+//opening tree
+//format:
+/*	
+{
+	"name": "id",
+	"children": [
+		{...},{...},{...}
+	]
+}
+//TODO, split out extended data into parallel lookup table
+id:{
+	"opening_groups":["A","B","C"],	//custom additions...
+	"opening_ending":"E01",
+	"fen":"",
+	"weight":1 //how many times in the creation process the node has been visited
+}
+*/
+var util = require('util');
+var fs = require('fs');
+var chess_utils = require('chess_utils');
+
+var start_time = new Date().valueOf()/1000;
+
+var openings_spreadsheet = '../pgn/chess_openings_adjusted.csv';
+var openings_text = fs.readFileSync(openings_spreadsheet,'utf8');
+var openings_list = d3.csv.parse(openings_text);
+
+util.puts("starting v2 (t:"+start_time+")");
+//generate the tree
+var tree_data = JSON.stringify( chess_utils.build_move_tree(openings_list) );
+//write out the move tree
+var out_file = fs.openSync('../generated_data/d3_move_tree.json', 'w');
+	fs.writeSync(out_file, tree_data);
+	fs.closeSync(out_file);
+
+out_file = fs.openSync('../generated_data/d3_move_tree.live.json', 'w');
+	fs.writeSync(out_file, "var openings_tree = " + tree_data);
+	fs.closeSync(out_file);
+
+util.puts("done in " + (( new Date().valueOf()/1000 ) - start_time ) + " s");
